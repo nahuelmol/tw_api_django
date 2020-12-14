7cf3982086ae916ca5a49e02926097ca920ddb9a
_moliNasil <41929823+NahuelMolina@users.noreply.github.com>
2020-12-13 23:24:36 -0300

    Update models.py

--- api_consumer/models.py
+++ api_consumer/models.py
@@ -5,8 +5,8 @@ class ClassName(models.Model):
 
 class LaunchesSpaceX(models.Model):
 	mission_name = models.TextField()
-	#launch_success = models.BooleanField(default=True)
-	#upcoming = models.BooleanField(default=False)
-	#launch_site = models.TextField()
-	#rocket_name = models.TextField()
-	#yt_video_link = models.CharField(max_length=25)
+	launch_success = models.BooleanField(default=True)
+	upcoming = models.BooleanField(default=False)
+	launch_site = models.TextField()
+	rocket_name = models.TextField()
+	yt_video_link = models.CharField(max_length=25)
